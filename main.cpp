18666a55d36afa751b312d862f2269554b3d3b7b
Tracy Johnson <0919346@lbccd.lbcc.cc.ca.us>
2022-04-18 23:23:42 -0700

    Initial commit to identify WGU student in new project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*
+    Print out to the screen, via your application, the course title, 
+    the programming language used, you WGU student ID, and your name.
+    */
+    string courseTitle = "Scripting and Programming - Applications - C867";
+    string programmingLanguage = "C++";
+    string wguStudentId = "000276959";
+    string studentName = "Tracy Johnson";
+
+    cout << courseTitle << endl;
+    cout << programmingLanguage << endl;
+    cout << wguStudentId << endl;
+    cout << studentName << endl;
+}
